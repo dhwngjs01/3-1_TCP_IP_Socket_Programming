195e97149c76d7ff0b16e8952a69a5f8ae7e8722
dhwngjs01 <dhwngjs01@naver.com>
2023-06-11 21:21:46 +0900

    Prettier

--- 20230508_echo/echo_client.c
+++ 20230508_echo/echo_client.c
@@ -8,45 +8,50 @@
 #define BUF_SIZE 1024
 void error_handling(char *message);
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     int sock;
     char message[BUF_SIZE];
     int str_len;
     struct sockaddr_in serv_adr, from_adr;
 
-    if(argc != 3){
+    if (argc != 3)
+    {
         printf("Usage: %s <IP> <port>\n", argv[0]);
         exit(1);
     }
 
     // 1. socket()
     sock = socket(PF_INET, SOCK_STREAM, 0);
-    if(sock == -1)
+    if (sock == -1)
         error_handling("socket() error");
-    
 
     // 2. connect()
-    memset(&serv_adr, 0, sizeof(serv_adr)); // 0으로 초기화
-    serv_adr.sin_family = AF_INET; // IPv4
+    memset(&serv_adr, 0, sizeof(serv_adr));        // 0으로 초기화
+    serv_adr.sin_family = AF_INET;                 // IPv4
     serv_adr.sin_addr.s_addr = inet_addr(argv[1]); // IP 주소
-    serv_adr.sin_port = htons(atoi(argv[2])); // 포트번호
+    serv_adr.sin_port = htons(atoi(argv[2]));      // 포트번호
 
-    if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1){
+    if (connect(sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
+    {
         error_handling("connect() error");
-    } else {
+    }
+    else
+    {
         puts("Connected...........");
     }
 
     // 3. read(), write()
-    while(1){
+    while (1)
+    {
         fputs("Input message(Q to quit): ", stdout);
         fgets(message, BUF_SIZE, stdin);
 
-        if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+        if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
             break;
 
         write(sock, message, strlen(message));
-        str_len = read(sock, message, BUF_SIZE-1);
+        str_len = read(sock, message, BUF_SIZE - 1);
         message[str_len] = 0;
         printf("Message from server: %s", message);
     }
@@ -55,7 +60,8 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-void error_handling(char *message){
+void error_handling(char *message)
+{
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
--- 20230515_uecho/uecho_server.c
+++ 20230515_uecho/uecho_server.c
@@ -8,37 +8,41 @@
 #define BUF_SIZE 30
 void error_handling(char *message);
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     int serv_sock;
     char message[BUF_SIZE];
     int str_len;
     socklen_t clnt_adr_sz;
 
     struct sockaddr_in serv_adr, clnt_adr;
-    if(argc != 2){
+    if (argc != 2)
+    {
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
 
     serv_sock = socket(PF_INET, SOCK_DGRAM, 0);
-    if(serv_sock == -1){
+    if (serv_sock == -1)
+    {
         error_handling("UDP socket creation error");
     }
 
     memset(&serv_adr, 0, sizeof(serv_adr));
-    serv_adr.sin_family = AF_INET; // IPv4
+    serv_adr.sin_family = AF_INET;                // IPv4
     serv_adr.sin_addr.s_addr = htonl(INADDR_ANY); // 32bit IPv4 address
-    serv_adr.sin_port = htons(atoi(argv[1])); // port
+    serv_adr.sin_port = htons(atoi(argv[1]));     // port
 
-    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1){
+    if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
+    {
         error_handling("bind() error");
     }
 
-    while(1){
+    while (1)
+    {
         clnt_adr_sz = sizeof(clnt_adr);
-        str_len = recvfrom(serv_sock, message, BUF_SIZE, 0, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
-        sendto(serv_sock, message, str_len, 0, (struct sockaddr*)&clnt_adr, clnt_adr_sz);
+        str_len = recvfrom(serv_sock, message, BUF_SIZE, 0, (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
+        sendto(serv_sock, message, str_len, 0, (struct sockaddr *)&clnt_adr, clnt_adr_sz);
 
         message[str_len] = 0;
         printf("%s \n", message);
@@ -47,7 +51,8 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-void error_handling(char *message){
+void error_handling(char *message)
+{
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
